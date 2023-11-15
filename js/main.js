871ee84d1fa251b7aae3e98d5ceb848d26e70d2b
buonoalex <loopatthecity@gmail.com>
2023-11-15 13:13:21 +0100

    finish logic part

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Questo è il file che contiene tutto il codice JS della pagina web
+
+//Selezione Fizz Buss for + if
+for(let i = 1; i<101; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("Fizz Buzz");
+    }else if(i % 3 == 0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
