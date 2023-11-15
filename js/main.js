da24fefca72c0b8d96492fd0e64033d454fc9078
buonoalex <loopatthecity@gmail.com>
2023-11-15 17:39:10 +0100

    Finish work now

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
 
 //Selezione Fizz Buss for + if
-for(let i = 1; i<101; i++){
+/*for(let i = 1; i<101; i++){
     if (i % 3 == 0 && i % 5 == 0){
         console.log("Fizz Buzz");
     }else if(i % 3 == 0){
@@ -11,7 +11,7 @@ for(let i = 1; i<101; i++){
     }else{
         console.log(i);
     }
-}
+}*/
 
 function bottonePrimaPagina(){
     let nomeGiocatore = document.getElementById("nomeGiocatore").value;
@@ -39,4 +39,44 @@ function tornaAlMenu(){
 function bottoneInzioGiocoCollezioneFizzBuzz(){
     document.getElementById("collezioneFizzBuzz").classList.add("d-none");
     document.getElementById("risultatoCollezioneFizzBuzz").classList.remove("d-none");
+
+    let numeroOccorrenzeGiocatore = document.getElementById("numeroOccorrenzeFizzBuzz").value;
+    let numeroOccorrenzeFizzBuzzRangeMin = document.getElementById("numeroOccorrenzeFizzBuzzRangeMin").value;
+    let numeroOccorrenzeFizzBuzzRangeMax = document.getElementById("numeroOccorrenzeFizzBuzzRangeMax").value;
+
+    console.log("Dati presi");
+    console.log(numeroOccorrenzeGiocatore);
+    console.log(numeroOccorrenzeFizzBuzzRangeMin);
+    console.log(numeroOccorrenzeFizzBuzzRangeMax);
+    let conteggioFizzBuzz = 0;
+    
+    for(let i=numeroOccorrenzeFizzBuzzRangeMin; i<numeroOccorrenzeFizzBuzzRangeMax; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            conteggioFizzBuzz = conteggioFizzBuzz + 1;
+        }
+        console.log("Completato");
+        console.log(conteggioFizzBuzz);
+    }
+
+    document.getElementById("numeroRangeMinGioco").innerHTML = numeroOccorrenzeFizzBuzzRangeMin;
+    document.getElementById("numeroRangeMaxGioco").innerHTML = numeroOccorrenzeFizzBuzzRangeMax;
+    document.getElementById("risultatoOccorrenzeGioco").innerHTML = conteggioFizzBuzz;
+    document.getElementById("contenitoreNumeroOccorrenzeGiocatore").innerHTML = numeroOccorrenzeGiocatore;
+
+    if(numeroOccorrenzeGiocatore == conteggioFizzBuzz){
+        document.getElementById("risultatoVittoriaSconfitta").innerHTML = "HAI VINTO!";
+    }else{
+        document.getElementById("risultatoVittoriaSconfitta").innerHTML = "HAI PERSO!";
+    }
+}
+
+function rigiocaCollezioneFizzBuzz(){
+    document.getElementById("risultatoCollezioneFizzBuzz").classList.add("d-none");
+    document.getElementById("collezioneFizzBuzz").classList.remove("d-none");
+}
+
+function tornaAlMenuDaCollezioneFizzBuzz(){
+    document.getElementById("risultatoCollezioneFizzBuzz").classList.add("d-none");
+    document.getElementById("collezioneFizzBuzz").classList.add("d-none");
+    document.getElementById("paginaMenu").classList.remove("d-none");
 }
